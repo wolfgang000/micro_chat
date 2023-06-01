a7deb661fcd79e23185390dcc5d36aa866543f43
wolfgang000 <wolfgangsalazarmora@gmail.com>
2023-06-01 15:19:15 -0500

    add docker compose dev (#12)
    * add docker dev backend
    
    * add docker dev frontend
    
    * fix

---
+++ backend/lib/micro_chat_web/healthchecks.ex
@@ -0,0 +1,5 @@
+defmodule MicroChatWeb.Healthchecks do
+  # Add db check here when a db is in use
+
+  def dummy_check, do: :ok
+end
--- backend/mix.exs
+++ backend/mix.exs
@@ -48,6 +48,7 @@ defmodule MicroChat.MixProject do
       {:phoenix_live_dashboard, "~> 0.7.2"},
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
+      {:plug_checkup, "~> 0.6.0"},
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"}
