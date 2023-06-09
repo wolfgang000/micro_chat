6b9b95da3435e708b7db23c5a36cb1de08a098bc
wolfgang000 <wolfgangsalazarmora@gmail.com>
2023-06-08 23:54:54 -0500

    wip

--- frontend/src/components/chat-room/Video.vue
+++ frontend/src/components/chat-room/Video.vue
@@ -32,79 +32,6 @@ onMounted(async () => {
 
   if (roomStorePinia.wasVideoActivateByCurrentUser) {
     channel.push('start_call', {})
-
-    channel.on(
-      `offer:${userStore.username}`,
-      async ({
-        offer: offer,
-        ice_candidates: peerIceCandidates,
-        username: username
-      }: {
-        offer: any
-        ice_candidates: any[]
-        username: string
-      }) => {
-        // asume 1 connection
-        const localStream = await promise
-        const peer = {
-          remoteStream: new MediaStream(),
-          username,
-          pc: new RTCPeerConnection(servers),
-          element_id: `remote-user-${username}`
-        }
-
-        // Push tracks from your local stream to the peer connection
-        localStream.getTracks().forEach((track) => {
-          peer.pc.addTrack(track, localStream)
-        })
-
-        // Set up an event listener to pull tracks from the remote peer stream when they are available
-
-        peer.pc.ontrack = (event) => {
-          event.streams[0].getTracks().forEach((track) => {
-            peer.remoteStream.addTrack(track)
-          })
-        }
-
-        peers.value.push(peer)
-        await nextTick()
-        const videoRemoteUser = document.getElementById(peer.element_id) as HTMLVideoElement
-        if (videoRemoteUser) {
-          videoRemoteUser.srcObject = peer.remoteStream
-        }
-
-        const iceCandidatesPromise = new Promise<RTCIceCandidate[]>((resolve, reject) => {
-          const candidates: RTCIceCandidate[] = []
-          peer.pc.onicecandidate = (event) => {
-            if (event.candidate) {
-              candidates.push(event.candidate)
-            } else {
-              resolve(candidates)
-            }
-          }
-        })
-
-        await peer.pc.setRemoteDescription(new RTCSessionDescription(offer))
-        const answerDescription = await peer.pc.createAnswer()
-        await peer.pc.setLocalDescription(answerDescription)
-
-        // Create an object which contains the SDP answer data
-        const iceCandidates = await iceCandidatesPromise
-        const answer = {
-          type: answerDescription.type,
-          sdp: answerDescription.sdp
-        }
-
-        // Send the answer to the signaling server which further sends it to the caller
-        channel.push(`answer:${peer.username}`, { answer, ice_candidates: iceCandidates })
-
-        peerIceCandidates.forEach((candidate) => {
-          console.log('Adding caller ice candidate', candidate)
-          const ice_candidate = new RTCIceCandidate(candidate)
-          peer.pc.addIceCandidate(ice_candidate)
-        })
-      }
-    )
   } else {
     const localStream = await promise
     const inCallUsers = roomStore.connectedUsers.filter((user) => user.is_in_call)
@@ -157,30 +84,103 @@ onMounted(async () => {
         channel.push(`offer:${peer.username}`, { offer: offer, ice_candidates: iceCandidates })
       })
     })
-
-    channel.on(
-      `answer:${userStore.username}`,
-      ({
-        answer,
-        ice_candidates: peerIceCandidates,
-        username
-      }: {
-        answer: any
-        ice_candidates: any[]
-        username: string
-      }) => {
-        const peer = peers.value.find((peer) => peer.username === username)!
-
-        peerIceCandidates.forEach((candidate) => {
-          const ice_candidate = new RTCIceCandidate(candidate)
-          peer.pc.addIceCandidate(ice_candidate)
-        })
-
-        const answerDescription = new RTCSessionDescription(answer)
-        peer.pc.setRemoteDescription(answerDescription)
-      }
-    )
   }
+
+  channel.on(
+    `offer:${userStore.username}`,
+    async ({
+      offer: offer,
+      ice_candidates: peerIceCandidates,
+      username: username
+    }: {
+      offer: any
+      ice_candidates: any[]
+      username: string
+    }) => {
+      // asume 1 connection
+      const localStream = await promise
+      const peer = {
+        remoteStream: new MediaStream(),
+        username,
+        pc: new RTCPeerConnection(servers),
+        element_id: `remote-user-${username}`
+      }
+
+      // Push tracks from your local stream to the peer connection
+      localStream.getTracks().forEach((track) => {
+        peer.pc.addTrack(track, localStream)
+      })
+
+      // Set up an event listener to pull tracks from the remote peer stream when they are available
+
+      peer.pc.ontrack = (event) => {
+        event.streams[0].getTracks().forEach((track) => {
+          peer.remoteStream.addTrack(track)
+        })
+      }
+
+      peers.value.push(peer)
+      await nextTick()
+      const videoRemoteUser = document.getElementById(peer.element_id) as HTMLVideoElement
+      if (videoRemoteUser) {
+        videoRemoteUser.srcObject = peer.remoteStream
+      }
+
+      const iceCandidatesPromise = new Promise<RTCIceCandidate[]>((resolve, reject) => {
+        const candidates: RTCIceCandidate[] = []
+        peer.pc.onicecandidate = (event) => {
+          if (event.candidate) {
+            candidates.push(event.candidate)
+          } else {
+            resolve(candidates)
+          }
+        }
+      })
+
+      await peer.pc.setRemoteDescription(new RTCSessionDescription(offer))
+      const answerDescription = await peer.pc.createAnswer()
+      await peer.pc.setLocalDescription(answerDescription)
+
+      // Create an object which contains the SDP answer data
+      const iceCandidates = await iceCandidatesPromise
+      const answer = {
+        type: answerDescription.type,
+        sdp: answerDescription.sdp
+      }
+
+      // Send the answer to the signaling server which further sends it to the caller
+      channel.push(`answer:${peer.username}`, { answer, ice_candidates: iceCandidates })
+
+      peerIceCandidates.forEach((candidate) => {
+        console.log('Adding caller ice candidate', candidate)
+        const ice_candidate = new RTCIceCandidate(candidate)
+        peer.pc.addIceCandidate(ice_candidate)
+      })
+    }
+  )
+
+  channel.on(
+    `answer:${userStore.username}`,
+    ({
+      answer,
+      ice_candidates: peerIceCandidates,
+      username
+    }: {
+      answer: any
+      ice_candidates: any[]
+      username: string
+    }) => {
+      const peer = peers.value.find((peer) => peer.username === username)!
+
+      peerIceCandidates.forEach((candidate) => {
+        const ice_candidate = new RTCIceCandidate(candidate)
+        peer.pc.addIceCandidate(ice_candidate)
+      })
+
+      const answerDescription = new RTCSessionDescription(answer)
+      peer.pc.setRemoteDescription(answerDescription)
+    }
+  )
 })
 </script>
 
