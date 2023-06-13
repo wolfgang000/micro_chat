5ed380e6b269d3bb361258d34f4ddcefa3c470c1
wolfgang000 <wolfgangsalazarmora@gmail.com>
2023-06-13 13:42:36 -0500

    add feature flag

--- frontend/src/env.ts
+++ frontend/src/env.ts
@@ -1,11 +1,13 @@
 const websocket_base_url = import.meta.env.VITE_WEBSOCKET_BASE_URL
+const feature_flag_videocall = import.meta.env.VITE_FEATURE_FLAG_VIDEOCALL
 
 if (!websocket_base_url) {
   throw new Error('Environment variable WEBSOCKET_BASE_URL is missing.')
 }
 
 const env = {
-  websocket_base_url: websocket_base_url
+  websocket_base_url: websocket_base_url,
+  feature_flag_videocall: feature_flag_videocall || false
 }
 
 export default env
